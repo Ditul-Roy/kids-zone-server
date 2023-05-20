21f72a44f36c02e322b98eb2ebce87f9687be0f5
Ditul-Roy <ditulr@gmail.com>
2023-05-20 21:59:06 +0600

    vercel config remove and .env file added in gitignore

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    client.connect()
 
     const carCollection = client.db('carszoneDB').collection('carzone');
     const otherCollection = client.db('carszoneDB').collection('others');
