28f4dcdd6b5875208289ab0c5208331831707883
Ditul-Roy <ditulr@gmail.com>
2023-05-23 00:31:44 +0600

    last updated

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
     app.get('/mycars/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
-      const carrResult = await carCollection.findOne(query);
+      const carrResult = await carCollection.findOne(query).sort({price:1});
       res.send(carrResult)
     })
 
