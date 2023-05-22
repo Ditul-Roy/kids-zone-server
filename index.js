e3b35f222e5e9fca0bab61fa99ec17dfe83f2f9c
Ditul-Roy <ditulr@gmail.com>
2023-05-22 22:49:39 +0600

    some updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) =>{
     res.send('kids car server is running')
 })
 
-const uri = `mongodb+srv://carToys:XNlx1hIG0d4lEDmf@cluster0.bylwpc6.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bylwpc6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
